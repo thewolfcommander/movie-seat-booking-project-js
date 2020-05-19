144bc8d71a69ef84295bd225a6c5ff1fa332862b
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-05-20 00:52:40 +0530

    Basic part of the app completed

--- app.js
+++ app.js
@@ -1,27 +1,65 @@
 const container = document.querySelector('.container');
-const seat = document.querySelectorAll('.seat .seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 
 const movie = document.getElementById('movie');
+
+
+populateUI();
 let ticketPrice = +movie.value;
 
 const count = document.getElementById('count')
 const total = document.getElementById('total')
 
+
+
+// Setting Selected Movie information to the localstorage
+const setSelectedMovie = (movieIndex, moviePrice) => {
+    localStorage.setItem('selectedMovie', movieIndex);
+    localStorage.setItem('selectedPrice', moviePrice);
+}
+
+
 // Function to update count
 const updateCount = () => {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
     count.innerText = selectedSeats.length;
     total.innerText = selectedSeats.length * ticketPrice;
 }
 
+// Populating UI with local storage data
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !== null && selectedSeats.length !== 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovie');
+
+    if (selectedMovieIndex !== null) {
+        movie.selectedIndex = selectedMovieIndex
+    }
+
+}
+
 
 // Function for encountering price change of ticket
 movie.addEventListener('change', e => {
-    ticketPrice = e.target.value;
+    ticketPrice = +e.target.value;
 
+    setSelectedMovie(e.target.selectedIndex, e.target.value);
     updateCount();
 })
+
 
 
 // Event listener for click on seat
@@ -32,3 +70,5 @@ container.addEventListener('click', e => {
         updateCount();
     }
 })
+
+updateCount()
